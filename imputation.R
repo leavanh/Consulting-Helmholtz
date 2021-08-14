f74d35a4a38842b9991dcde5052028e8382907fc
Maren Kellerer <Maren.Kellerer@gmail.com>
2021-08-14 12:12:37 +0200

    complete_evaluation instead of comp

--- imputation.R
+++ imputation.R
@@ -20,10 +20,10 @@ mice_data <- mice_data[c("h_index", "impact_factor", "gender", "length",
 
 # delete obs from the full dataset
 deleted_obs <- delete_obs(mice_data, 
-                          var_w_missings = c("h_index", "impact_factor", 
-                                             "length",
-                                             "gender","journal_subject"),
-                          perc = c(0.03, 0.06, 0.4, 0.21, 0.01))
+                          var_with_missings = c("h_index", "impact_factor", 
+                                                "length", "gender",
+                                                "journal_subject"),
+                          percentage = c(0.03, 0.06, 0.4, 0.21, 0.01))
 
 data_missings <- deleted_obs$data_missings
 rows <- deleted_obs$rows
@@ -40,10 +40,9 @@ saveRDS(imp, "./Data/imp.RDS") # save
 
 # get diagnostic plots for cart
 
-mice_w_diag(data_missings, data_no_NA, rows,
-            var_cont = c("h_index", "impact_factor", "length"),
-            var_cat = c("gender", "journal_subject"),
-            maxit = 5, seed = 500, meth = 'cart', print = F)
+mice_with_diagnostic(data_missings, data_no_NA, rows,
+                     var_continuous = c("h_index", "impact_factor", "length"),
+                     maxit = 5, seed = 500, meth = 'cart', print = F)
 
 densityplot(imp)
 stripplot(imp)
@@ -87,11 +86,11 @@ eval12 <- get_evaluation(mice(data_missings,
                              defaultMethod = c("cart", "logreg.boot", "polyreg",
                                                "polr"), print = FALSE))
 
-comp <- rbind(eval1, eval2, eval3, eval4, eval5, eval6, eval7, eval8, eval9,
-              eval10, eval11, eval12)
+complete_evaluation <- rbind(eval1, eval2, eval3, eval4, eval5, eval6, eval7, 
+                             eval8, eval9, eval10, eval11, eval12)
 
 # make one table
-comp <- mutate(as.data.frame(comp),
+complete_evaluation <- mutate(as.data.frame(complete_evaluation),
                h_index = format(round(h_index, 2), nsmall = 2),
                impact_factor = format(round(impact_factor, 2), nsmall = 2),
                length = format(round(length, 2), nsmall = 2),
@@ -100,42 +99,43 @@ comp <- mutate(as.data.frame(comp),
                journal_subject = paste(as.character(format(round(
                  journal_subject, 2), nsmall = 2)), "%"))
 
-rownames(comp) <- c()
-colnames(comp) <- c("Correlation\n h-index", "Correlation\n impact factor",
-                    "Correlation\n length",
-                    "Percentage\n gender", "Percentage\n journal subject")
-comp <- cbind("method" = c("pmm", "midastouch", "rf", "cart",
-                           "pmm, logreg, polyreg, polr",
-                           "pmm, logreg.boot, polyreg, polr",
-                           "midastouch, logreg, polyreg, polr",
-                           "midastouch, logreg.boot, polyreg, polr",
-                           "rf, logreg, polyreg, polr",
-                           "rf, logreg.boot, polyreg, polr",
-                           "cart, logreg, polyreg, polr",
-                           "cart, logreg.boot, polyreg, polr"),
-              "method numeric" = c("pmm", "midastouch", "rf", "cart",
-                                   "pmm",
-                                   "pmm",
-                                   "midastouch",
-                                   "midastouch",
-                                   "rf",
-                                   "rf",
-                                   "cart",
-                                   "cart"),
-              "method binary" = c("pmm", "midastouch", "rf", "cart",
-                                  "logreg",
-                                  "logreg.boot",
-                                  "logreg",
-                                  "logreg.boot",
-                                  "logreg",
-                                  "logreg.boot",
-                                  "logreg",
-                                  "logreg.boot"),
-              "method categorical" = c("pmm", "midastouch", "rf", "cart",
-                                       rep("polyreg", 8)),
-              comp)
-comp <- select(comp, -method)
+rownames(complete_evaluation) <- c()
+colnames(complete_evaluation) <- c("Correlation\n h-index", 
+                                   "Correlation\n impact factor",
+                                   "Correlation\n length",
+                                   "Percentage\n gender", "Percentage\n journal subject")
+complete_evaluation <- cbind("method" = c("pmm", "midastouch", "rf", "cart",
+                                           "pmm, logreg, polyreg, polr",
+                                           "pmm, logreg.boot, polyreg, polr",
+                                           "midastouch, logreg, polyreg, polr",
+                                           "midastouch, logreg.boot, polyreg, polr",
+                                           "rf, logreg, polyreg, polr",
+                                           "rf, logreg.boot, polyreg, polr",
+                                           "cart, logreg, polyreg, polr",
+                                           "cart, logreg.boot, polyreg, polr"),
+                            "method numeric" = c("pmm", "midastouch", "rf", "cart",
+                                                 "pmm",
+                                                 "pmm",
+                                                 "midastouch",
+                                                 "midastouch",
+                                                 "rf",
+                                                 "rf",
+                                                 "cart",
+                                                 "cart"),
+                            "method binary" = c("pmm", "midastouch", "rf", "cart",
+                                                "logreg",
+                                                "logreg.boot",
+                                                "logreg",
+                                                "logreg.boot",
+                                                "logreg",
+                                                "logreg.boot",
+                                                "logreg",
+                                                "logreg.boot"),
+                            "method categorical" = c("pmm", "midastouch", "rf", "cart",
+                                                     rep("polyreg", 8)),
+                            complete_evaluation)
+complete_evaluation <- select(complete_evaluation, -method)
   
 pdf("./Plots/mice_comp.pdf", height = 8, width = 12)
-grid.table(comp)
+grid.table(complete_evaluation)
 dev.off()
